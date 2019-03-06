74ca455146ef56c337e2bfdc72f780a0fc7b01e0
Michal Borejszo <michal@traal.eu>
2019-03-06 18:15:44 +0100

    Added 'WriteSingleRegister' and 'WriteMultipleRegisters'

--- PsModbusTcp.ps1
+++ PsModbusTcp.ps1
@@ -4,34 +4,47 @@ enum ModbusFunction {
     ReadHoldingRegisters = [byte]0x03
     ReadInputRegisters = [byte]0x04
     WriteSingleRegister = [byte]0x06
+    WriteMultipleRegisters = [byte]0x10
 }
 
 function Send-ModbusCommand {
     param (
-        [Parameter(Mandatory=$true)][ModbusFunction]$function,
-        [Parameter(Mandatory=$true)][string]$address,
-        [Parameter(Mandatory=$true)][uint16]$port,
-        [Parameter(Mandatory=$true)][uint16]$reference,
-        [byte]$slave = 1,
-        [uint16]$num = 0
+        [Parameter(Mandatory=$true)][ModbusFunction]$Function,
+        [Parameter(Mandatory=$true)][string]$Address,
+        [Parameter(Mandatory=$true)][uint16]$Port,
+        [Parameter(Mandatory=$true)][uint16]$Reference,
+        [Parameter(Mandatory=$true)][uint16[]]$Payload,
+        [byte]$Slave = 1
     )
 
-    $tcpConnection = New-Object System.Net.Sockets.TcpClient($address, $port)
-    $tcpStream = $tcpConnection.GetStream()
+    $ReferenceBytes = [System.BitConverter]::GetBytes($Reference)
+    [array]::Reverse($ReferenceBytes)
 
-    $referenceBytes = [System.BitConverter]::GetBytes($reference)
-    [array]::Reverse($referenceBytes)
-
-    $numBytes = [System.BitConverter]::GetBytes($num)
-    [array]::Reverse($numBytes)
-
-    [byte[]]$tail = $slave,   # unit id
-                    $function # function id
-
-    $tail += $referenceBytes
-    if ($function -eq [ModbusFunction]::ReadHoldingRegisters -or $function -eq [ModbusFunction]::ReadInputRegisters)
+    [byte[]]$PayloadBytes = @(0) * ($Payload.Length * 2)
+    for ($i = 0; $i -lt $Payload.Length; $i++)
     {
-        $tail += $numBytes
+        $p = $Payload[$i]
+        $pBytes = [System.BitConverter]::GetBytes($p)
+        [array]::Reverse($pBytes)
+        [array]::Copy($pBytes, 0, $PayloadBytes, $i *2, 2)
+    }
+
+    [byte[]]$tail = $Slave,   # unit id
+                    $Function # function id
+
+    $tail += $ReferenceBytes
+    if ($PayloadBytes.Length -gt 0)
+    {
+        if ($Function -eq [ModbusFunction]::WriteMultipleRegisters)
+        {
+            # add word count
+            [uint16]$wordCount = $Payload.Length
+            $wordCountBytes = [System.BitConverter]::GetBytes($wordCount)
+            [array]::Reverse($wordCountBytes)
+            $tail += $wordCountBytes
+            $tail += [byte]$PayloadBytes.Length # byte count
+        }
+        $tail += $PayloadBytes
     }
 
     [uint16]$length = $tail.Length
@@ -46,6 +59,9 @@ function Send-ModbusCommand {
 
     [byte[]] $buffer = @(0) * 30
 
+    $tcpConnection = New-Object System.Net.Sockets.TcpClient($Address, $Port)
+    $tcpConnection.ReceiveTimeout = 5000
+    $tcpStream = $tcpConnection.GetStream()
     $tcpStream.Write($data,0,$data.length)
     $tcpStream.Flush()
     $size = $tcpStream.Read($buffer,0,$buffer.length)
@@ -61,22 +77,22 @@ function Send-ModbusCommand {
 
 function Read-Registers {
     param (
-        [Parameter(Mandatory=$true)][ModbusFunction]$function,
-        [Parameter(Mandatory=$true)][string]$address,
-        [Parameter(Mandatory=$true)][uint16]$port,
-        [Parameter(Mandatory=$true)][uint16]$reference,
-        [Parameter(Mandatory=$true)][uint16]$num,
-        [byte]$slave = 1
+        [Parameter(Mandatory=$true)][ModbusFunction]$Function,
+        [Parameter(Mandatory=$true)][string]$Address,
+        [Parameter(Mandatory=$true)][uint16]$Port,
+        [Parameter(Mandatory=$true)][uint16]$Reference,
+        [Parameter(Mandatory=$true)][uint16]$Num,
+        [byte]$Slave = 1
     )
     
-    if ($function -ne [ModbusFunction]::ReadHoldingRegisters -and $function -ne [ModbusFunction]::ReadInputRegisters)
+    if ($Function -ne [ModbusFunction]::ReadHoldingRegisters -and $Function -ne [ModbusFunction]::ReadInputRegisters)
     {
         Write-Error "Function has to be one of the 'read registers' ones"
         return $null
     }
 
-    $result = Send-ModbusCommand $function $address $port $reference $slave $num
-    if ($result[7] -ne $function)
+    $result = Send-ModbusCommand $Function $Address $Port $Reference @($Num) $Slave
+    if ($result[7] -ne $Function)
     {
         Write-Error "Response error " + $result[7]
         return $null
@@ -96,24 +112,56 @@ function Read-Registers {
 
 function Read-HoldingRegisters {
     param (
-        [Parameter(Mandatory=$true)][string]$address,
-        [Parameter(Mandatory=$true)][uint16]$port,
-        [Parameter(Mandatory=$true)][uint16]$reference,
-        [Parameter(Mandatory=$true)][uint16]$num,
-        [byte]$slave = 1
+        [Parameter(Mandatory=$true)][string]$Address,
+        [Parameter(Mandatory=$true)][uint16]$Port,
+        [Parameter(Mandatory=$true)][uint16]$Reference,
+        [Parameter(Mandatory=$true)][uint16]$Num,
+        [byte]$Slave = 1
     )
     
-    return Read-Registers ReadHoldingRegisters $address $port $reference $num $slave
+    return Read-Registers ReadHoldingRegisters $Address $Port $Reference $Num $Slave
 }
 
 function Read-InputRegisters {
     param (
-        [Parameter(Mandatory=$true)][string]$address,
-        [Parameter(Mandatory=$true)][uint16]$port,
-        [Parameter(Mandatory=$true)][uint16]$reference,
-        [Parameter(Mandatory=$true)][uint16]$num,
-        [byte]$slave = 1
+        [Parameter(Mandatory=$true)][string]$Address,
+        [Parameter(Mandatory=$true)][uint16]$Port,
+        [Parameter(Mandatory=$true)][uint16]$Reference,
+        [Parameter(Mandatory=$true)][uint16]$Num,
+        [byte]$Slave = 1
     )
     
-    return Read-Registers ReadInputRegisters $address $port $reference $num $slave
+    return Read-Registers ReadInputRegisters $Address $Port $Reference $Num $Slave
+}
+
+function Write-SingleRegister {
+    param (
+        [Parameter(Mandatory=$true)][string]$Address,
+        [Parameter(Mandatory=$true)][uint16]$Port,
+        [Parameter(Mandatory=$true)][uint16]$Reference,
+        [Parameter(Mandatory=$true)][uint16]$Data,
+        [byte]$Slave = 1
+    )
+    
+    $result = Send-ModbusCommand WriteSingleRegister $Address $Port $Reference @($Data) $Slave
+    if ($result[7] -ne [ModbusFunction]::WriteSingleRegister)
+    {
+        Write-Error "Response error " + $result[7]
+    }
+}
+
+function Write-MultipleRegisters {
+    param (
+        [Parameter(Mandatory=$true)][string]$Address,
+        [Parameter(Mandatory=$true)][uint16]$Port,
+        [Parameter(Mandatory=$true)][uint16]$Reference,
+        [Parameter(Mandatory=$true)][uint16[]]$Data,
+        [byte]$Slave = 1
+    )
+    
+    $result = Send-ModbusCommand WriteMultipleRegisters $Address $Port $Reference $Data $Slave
+    if ($result[7] -ne [ModbusFunction]::WriteMultipleRegisters)
+    {
+        Write-Error "Response error " + $result[7]
+    }
 }
